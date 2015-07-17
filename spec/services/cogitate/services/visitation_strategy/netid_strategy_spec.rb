21d97fa2e8b4052bcf60d3f515dcc26ed66d79e8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-07-17 12:24:28 -0400

    Improving code coverage to 100%
    This change highlights a fallacy. The units are working, however
    there is no coordination of functional testing.

--- spec/services/cogitate/services/visitation_strategy/netid_strategy_spec.rb
+++ spec/services/cogitate/services/visitation_strategy/netid_strategy_spec.rb
@@ -4,20 +4,29 @@ module Cogitate
   module Services
     module VisitationStrategy
       module NetidStrategy
+        RSpec.describe Base do
+          let(:identifier) { double(strategy: 'netid', identifying_value: '1234') }
+          let(:agent_builder) { double(add_identity: true, add_verified_authentication_vector: true) }
+          subject { described_class.new(identifier: identifier) }
+
+          it 'will raise a NotImplementedError for #receive' do
+            expect { subject.send(:receive, agent_builder) }.to raise_error(NotImplementedError)
+          end
+        end
         RSpec.describe Verified do
           let(:identifier) { double(strategy: 'netid', identifying_value: '1234') }
           let(:visitor) { double(visit: true) }
-          let(:agent) { double(add_identity: true, add_verified_authentication_vector: true) }
+          let(:agent_builder) { double(add_identity: true, add_verified_authentication_vector: true) }
           subject { described_class.new(identifier: identifier) }
-          before { allow(visitor).to receive(:visit).and_yield(agent) }
+          before { allow(visitor).to receive(:visit).and_yield(agent_builder) }
 
           context '#invite a visitor' do
             it 'will add the identity to the agent builder' do
-              expect(agent).to receive(:add_identity).with(identifier)
+              expect(agent_builder).to receive(:add_identity).with(identifier)
               subject.invite(visitor)
             end
             it 'will add to the verified_authentication_vectors of the agent builder' do
-              expect(agent).to receive(:add_verified_authentication_vector).with(identifier)
+              expect(agent_builder).to receive(:add_verified_authentication_vector).with(identifier)
               subject.invite(visitor)
             end
           end
@@ -26,17 +35,17 @@ module Cogitate
         RSpec.describe Unverified do
           let(:identifier) { double(strategy: 'netid', identifying_value: '1234') }
           let(:visitor) { double(visit: true) }
-          let(:agent) { double(add_identity: true, add_verified_authentication_vector: true) }
+          let(:agent_builder) { double(add_identity: true, add_verified_authentication_vector: true) }
           subject { described_class.new(identifier: identifier) }
-          before { allow(visitor).to receive(:visit).and_yield(agent) }
+          before { allow(visitor).to receive(:visit).and_yield(agent_builder) }
 
           context '#invite a visitor' do
             it 'will add the identity to the agent builder' do
-              expect(agent).to receive(:add_identity).with(identifier)
+              expect(agent_builder).to receive(:add_identity).with(identifier)
               subject.invite(visitor)
             end
             it 'will NOT dd to the verified_authentication_vectors of the agent builder' do
-              expect(agent).to_not receive(:add_verified_authentication_vector).with(identifier)
+              expect(agent_builder).to_not receive(:add_verified_authentication_vector).with(identifier)
               subject.invite(visitor)
             end
           end
