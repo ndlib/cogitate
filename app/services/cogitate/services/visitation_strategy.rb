b9526458a8979e52c7fac7ab7704cb339a2ab4d5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-07-20 10:22:09 -0400

    Removing concepts that are not applicable

--- app/services/cogitate/services/visitation_strategy.rb
+++
@@ -1,7 +0,0 @@
-module Cogitate
-  module Services
-    # Establishing a namespace for visitation strategy. A visitation strategy is responsible handling the invitation of a visitor.
-    module VisitationStrategy
-    end
-  end
-end
