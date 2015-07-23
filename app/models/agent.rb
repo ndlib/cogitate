43d49badfde050f7e68f645389cbeeee982e2318
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-07-23 08:36:54 -0400

    Updating Agent to include JSON_API info

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,4 +1,5 @@
 require 'cogitate/interfaces'
+require 'base64'
 
 # An Agent is a "bucket" of attributes. It represents a single acting entity:
 #
@@ -6,10 +7,13 @@ require 'cogitate/interfaces'
 # * a Service - an application that "does stuff" to data
 class Agent
   include Contracts
-  Contract(Contracts::None => Cogitate::Interfaces::AgentInterface)
-  def initialize(container: default_container)
+  # @note I'm choosing the :identifier as the input and :primary_identifier as the attribute. I believe it is possible that during the
+  #       process that builds the Agent, I may encounter a more applicable primary_identifier.
+  Contract(Contracts::KeywordArgs[identifier: Cogitate::Interfaces::IdentifierInterface] => Cogitate::Interfaces::AgentInterface)
+  def initialize(identifier:, container: default_container)
     self.identities = container.new
     self.verified_authentication_vectors = container.new
+    self.primary_identifier = identifier
     self
   end
 
@@ -19,9 +23,29 @@ class Agent
   # @return [Enumerable] These are identifiers (and associated information) that has been verified
   attr_reader :verified_authentication_vectors
 
+  # @return [Cogitate::Interfaces::IdentifierInterface] What has been assigned as the primary identifier of this agent.
+  attr_reader :primary_identifier
+
+  extend Forwardable
+  def_delegators :primary_identifier, :strategy, :identifying_value
+
+  JSON_API_TYPE = 'agents'.freeze
+  def type
+    JSON_API_TYPE
+  end
+
+  def id
+    # Pass this on to the identifier
+    # TODO: This is a property of the identifier
+    Base64.urlsafe_encode64("#{strategy}\t#{identifying_value}")
+  end
+
   private
 
   attr_writer :identities, :verified_authentication_vectors
+
+  Contract(Cogitate::Interfaces::IdentifierInterface => Contracts::Any)
+  attr_writer :primary_identifier
 
   def default_container
     require 'set' unless defined?(Set)
--- spec/models/agent_spec.rb
+++ spec/models/agent_spec.rb
@@ -1,13 +1,26 @@
 require 'spec_fast_helper'
-require 'cogitate/interfaces'
 require 'agent'
+require 'identifier'
+require 'shoulda/matchers'
+require 'base64'
 
 RSpec.describe Agent do
-  subject { Agent.new }
+  let(:identifier) { Identifier.new(strategy: 'orcid', identifying_value: '123') }
+  subject { Agent.new(identifier: identifier) }
 
   include Cogitate::RSpecMatchers
   it { should contractually_honor(Cogitate::Interfaces::AgentInterface) }
 
   its(:identities) { should be_a(Set) }
+  its(:primary_identifier) { should eq(identifier) }
   its(:verified_authentication_vectors) { should be_a(Set) }
+
+  it { should delegate_method(:strategy).to(:primary_identifier) }
+  it { should delegate_method(:identifying_value).to(:primary_identifier) }
+
+  its(:type) { should eq(described_class::JSON_API_TYPE) }
+
+  it 'will have a URL Safe Base64 encoded ID' do
+    expect { Base64.urlsafe_decode64(subject.id) }.to_not raise_error
+  end
 end
