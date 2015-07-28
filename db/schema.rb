4abf80607d4121d07321eaea163740e0791b116b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-07-28 08:44:01 -0400

    Adding RepositoryService concept

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150728172516) do
+
+  create_table "repository_service_identifier_relationships", force: :cascade do |t|
+    t.string   "left_strategy",           limit: 255, null: false
+    t.string   "left_identifying_value",  limit: 255, null: false
+    t.string   "right_strategy",          limit: 255, null: false
+    t.string   "right_identifying_value", limit: 255, null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "repository_service_identifier_relationships", ["left_strategy", "left_identifying_value", "right_strategy", "right_identifying_value"], name: "idx_rs_identifier_relationships_both", unique: true, using: :btree
+  add_index "repository_service_identifier_relationships", ["left_strategy", "left_identifying_value"], name: "idx_rs_identifier_relationships_left", using: :btree
+  add_index "repository_service_identifier_relationships", ["right_strategy", "right_identifying_value"], name: "idx_rs_identifier_relationships_right", using: :btree
 
 end
