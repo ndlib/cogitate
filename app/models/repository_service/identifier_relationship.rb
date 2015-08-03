73f86c0794e33d7528c8c731c5762d15abfcd346
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-03 10:31:24 -0400

    Expanding IdentifierRelationship query
    I want to be able to limit the scope of objects returned from the
    Identifier Relationship to just groups; As I'm working on things
    I'm thinking of Groups as a terminal point when I cascade finding
    relationships.

--- app/models/repository_service/identifier_relationship.rb
+++ app/models/repository_service/identifier_relationship.rb
@@ -8,25 +8,37 @@ module RepositoryService
   # to be passing this information around, instead preferring a PORO.
   class IdentifierRelationship < ActiveRecord::Base
     self.table_name = 'repository_service_identifier_relationships'
-
-    extend Contracts
     # Responsible for finding related identifiers by checking both "sides" (i.e. left and right) of the relationship.
     #
     # @api public
     # @param identifier [Identifier]
+    # @param strategy [String, nil] Limit the relationship to objects of the given strategy; If none is given there are no limits.
     # @return If a block is given, it will yield each identifier. Otherwise an Enumerator is returned (and you can use #each on
     #         that Enumerator).
     # @note This method violates Assignment Branch Condition complexity but I feel this is an acceptable compromise given
     #       that the other option is extracting two private methods and passing a lambda into each of those
-    def self.each_identifier_related_to(identifier:)
-      return enum_for(:each_identifier_related_to, identifier: identifier) unless block_given?
-      where(left_strategy: identifier.strategy, left_identifying_value: identifier.identifying_value).find_each do |relationship|
-        yield(Identifier.new(strategy: relationship.right_strategy, identifying_value: relationship.right_identifying_value))
-      end
-      where(right_strategy: identifier.strategy, right_identifying_value: identifier.identifying_value).find_each do |relationship|
-        yield(Identifier.new(strategy: relationship.left_strategy, identifying_value: relationship.left_identifying_value))
-      end
+    def self.each_identifier_related_to(identifier:, strategy: nil)
+      return enum_for(:each_identifier_related_to, identifier: identifier, strategy: strategy) unless block_given?
+      each_right_identifier_related_to(identifier: identifier, strategy: strategy) { |related_identifier| yield(related_identifier) }
+      each_left_identifier_related_to(identifier: identifier, strategy: strategy) { |related_identifier| yield(related_identifier) }
       nil
     end
+
+    # @api private
+    def self.each_left_identifier_related_to(identifier:, strategy: nil)
+      scope = strategy.nil? ? self : where(left_strategy: strategy)
+      scope.where(right_strategy: identifier.strategy, right_identifying_value: identifier.identifying_value).find_each do |relationship|
+        yield(Identifier.new(strategy: relationship.left_strategy, identifying_value: relationship.left_identifying_value))
+      end
+    end
+
+    # @api private
+    def self.each_right_identifier_related_to(identifier:, strategy: nil)
+      scope = strategy.nil? ? self : where(right_strategy: strategy)
+      scope.where(left_strategy: identifier.strategy, left_identifying_value: identifier.identifying_value).find_each do |relationship|
+        yield(Identifier.new(strategy: relationship.right_strategy, identifying_value: relationship.right_identifying_value))
+      end
+    end
+    private_class_method :each_left_identifier_related_to, :each_right_identifier_related_to
   end
 end
--- spec/models/repository_service/identifier_relationship_spec.rb
+++ spec/models/repository_service/identifier_relationship_spec.rb
@@ -13,18 +13,34 @@ module RepositoryService
       end
       let(:given_identifer) { Identifier.new(strategy: 'a', identifying_value: '1') }
 
-      it 'will return an Enumerator if no block is given' do
-        enumerator = described_class.each_identifier_related_to(identifier: given_identifer)
-        expect(enumerator).to be_a(Enumerator)
-        expect { |b| enumerator.each(&b) }.to yield_successive_args(
-          Identifier.new(strategy: 'b', identifying_value: '2'), Identifier.new(strategy: 'c', identifying_value: '3')
-        )
+      context 'without a given strategy' do
+        it 'will return an Enumerator if no block is given' do
+          enumerator = described_class.each_identifier_related_to(identifier: given_identifer)
+          expect(enumerator).to be_a(Enumerator)
+          expect { |b| enumerator.each(&b) }.to yield_successive_args(
+            Identifier.new(strategy: 'b', identifying_value: '2'), Identifier.new(strategy: 'c', identifying_value: '3')
+          )
+        end
+
+        it 'will yield each Identifier if a block is given' do
+          expect { |b| described_class.each_identifier_related_to(identifier: given_identifer, &b) }.to yield_successive_args(
+            Identifier.new(strategy: 'b', identifying_value: '2'), Identifier.new(strategy: 'c', identifying_value: '3')
+          )
+        end
       end
 
-      it 'will yield each Identifier if a block is given' do
-        expect { |b| described_class.each_identifier_related_to(identifier: given_identifer, &b) }.to yield_successive_args(
-          Identifier.new(strategy: 'b', identifying_value: '2'), Identifier.new(strategy: 'c', identifying_value: '3')
-        )
+      context 'with a given strategy' do
+        it 'will return an Enumerator if no block is given' do
+          enumerator = described_class.each_identifier_related_to(identifier: given_identifer, strategy: 'b')
+          expect(enumerator).to be_a(Enumerator)
+          expect { |b| enumerator.each(&b) }.to yield_successive_args(Identifier.new(strategy: 'b', identifying_value: '2'))
+        end
+
+        it 'will yield each Identifier if a block is given' do
+          expect do |b|
+            described_class.each_identifier_related_to(identifier: given_identifer, strategy: 'b', &b)
+          end.to yield_successive_args(Identifier.new(strategy: 'b', identifying_value: '2'))
+        end
       end
     end
   end
