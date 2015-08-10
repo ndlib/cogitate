f98c76a13dd4aa5817ac8a1b52f2cd8bea8e0964
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-10 14:53:50 -0400

    Adding Cogitate::Models

---
+++ lib/cogitate/models.rb
@@ -0,0 +1,6 @@
+module Cogitate
+  # The data structure and model definitions of Cogitate objects.
+  # Found within this module are models that are shared concepts between Client and Server.
+  module Models
+  end
+end
