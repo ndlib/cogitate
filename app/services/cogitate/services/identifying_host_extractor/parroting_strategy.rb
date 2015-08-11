2ef579d0ab8329bfda785f3ba95c7d5fdb7faebb
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-11 10:25:55 -0400

    Wordsmithing
    [skip ci]

--- app/services/cogitate/services/identifying_host_extractor/parroting_strategy.rb
+++ app/services/cogitate/services/identifying_host_extractor/parroting_strategy.rb
@@ -15,14 +15,17 @@ module Cogitate
           new(identifier: identifier)
         end
 
+        # Instantiate a ParrotingStrategy for visitation
+        #
         # @param identifier [Cogitate::Interfaces::IdentifierInterface]
         # @param identifier_builder [#call(identifier:)] I want make sure that I have an unverified identifier
         #
-        # @todo What if we already have a Identifier::Unverified
+        # @todo What if we already have a Identifier::Unverified?
         def initialize(identifier:, identifier_builder: default_identifier_builder)
           self.identifier = identifier_builder.call(identifier: identifier)
         end
 
+        # @api public
         def invite(guest)
           guest.visit(identifier) { |visitor| visitor.add_identifier(identifier) }
         end
