9120dee7c03df10380994597854522e0b4d0fe0b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-12 11:06:53 -0400

    Adding an actual agent_token to parse

--- spec/lib/cogitate/client/token_decoder_spec.rb
+++ spec/lib/cogitate/client/token_decoder_spec.rb
@@ -2,7 +2,7 @@ require 'spec_fast_helper'
 require 'cogitate/client/token_decoder'
 
 RSpec.describe Cogitate::Client::TokenDecoder do
-  let(:token) { double("Token") }
+  let(:token) { File.read(File.expand_path('../../../../fixtures/agent_token.jwt.txt', __FILE__)) }
   subject { described_class.new(token: token) }
   it 'will expose a .call as part of the public API' do
     expect_any_instance_of(described_class).to receive(:call)
