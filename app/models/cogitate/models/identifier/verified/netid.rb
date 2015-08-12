a63b42f731c259ff250f26d8aa0723c04508b3c5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-12 09:46:05 -0400

    Wordsmithing
    [skip ci]

--- app/models/cogitate/models/identifier/verified/netid.rb
+++ app/models/cogitate/models/identifier/verified/netid.rb
@@ -6,6 +6,8 @@ module Cogitate
       # :nodoc:
       module Verified
         Netid = Verified.build_named_strategy('first_name', 'last_name', 'netid', 'full_name', 'ndguid', 'email') do
+          # Making an assumption that all NetID's can send an email to netid@nd.edu
+          # It also appears that 'email' from the API server is coming back as blank
           def email
             "#{netid}@nd.edu"
           end
