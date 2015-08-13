035a4ca3e3bc5ce7b9443e01f786ac656c477095
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-13 09:37:42 -0400

    Updating application configuration

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,11 +1,10 @@
 Figaro.require_keys(
-  'agent_tokenizer_encryption_type',
-  'agent_tokenizer_private_password',
-  'agent_tokenizer_public_password',
-  'agent_tokenizer_issuer_claim',
+  'cogitate_services_tokenizer_encryption_type',
+  'cogitate_services_tokenizer_private_password',
+  'cogitate_services_tokenizer_public_password',
+  'cogitate_services_tokenizer_issuer_claim',
+  'cogitate_services_remote_server_base_url',
   'hesburgh_api_auth_token',
   'hesburgh_api_host',
-  'omniauth_cas_url',
-  'url_host',
-  'cogitate_services_remote_server_base_url'
+  'omniauth_cas_url'
 )
