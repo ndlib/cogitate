1b20b7857b7845ce248eb157be4ce4cbecb623b4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-13 15:55:23 -0400

    Adding find_current_identifier_for
    Persisting tickets and then reifying the object

--- app/models/cogitate/models/identifier_ticket.rb
+++ app/models/cogitate/models/identifier_ticket.rb
@@ -10,6 +10,16 @@ module Cogitate
       def self.create_ticket_from_identifier(identifier:, ticket:, expires_at: 10.minutes.from_now)
         self.create!(encoded_id: identifier.encoded_id, ticket: ticket, expires_at: expires_at)
       end
+
+      def self.find_current_identifier_for(ticket:, as_of: Time.zone.now, identifier_decoder: default_identifier_decoder)
+        identifier_ticket = where("ticket = :ticket AND expires_at > :as_of", ticket: ticket, as_of: as_of).first!
+        identifier_decoder.call(encoded_identifier: identifier_ticket.encoded_id)
+      end
+
+      def self.default_identifier_decoder
+        require 'cogitate/services/identifiers_decoder' unless defined? Services::IdentifiersDecoder
+        ->(encoded_identifier:) { Services::IdentifiersDecoder.call(encoded_identifier).first }
+      end
     end
   end
 end
--- spec/models/cogitate/models/identifier_ticket_spec.rb
+++ spec/models/cogitate/models/identifier_ticket_spec.rb
@@ -1,12 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Cogitate::Models::IdentifierTicket, type: :model do
-  context '#create_ticket_from_identifier' do
-    let(:identifier) { double('Identifier', encoded_id: '1234') }
+  let(:encoded_id) { "bmV0aWQJaHdvcmxk" }
+  let(:identifier) { double('Identifier', encoded_id: encoded_id) }
+  let(:ticket) { '123-456' }
+  context '.default_identifier_decoder' do
+    it 'will convert an encoded identifier' do
+      expect(described_class.default_identifier_decoder.call(encoded_identifier: encoded_id)).to be_a(Cogitate::Models::Identifier)
+    end
+  end
+  context '.create_ticket_from_identifier' do
     it 'will increment the count' do
       expect do
-        described_class.create_ticket_from_identifier(identifier: identifier, ticket: '123')
+        described_class.create_ticket_from_identifier(identifier: identifier, ticket: ticket)
       end.to change { described_class.count }.by(1)
+    end
+  end
+
+  context '.find_current_identifier_for' do
+    it 'find the given ticket and return an identifier' do
+      described_class.create_ticket_from_identifier(identifier: identifier, ticket: ticket)
+      expect(described_class.find_current_identifier_for(ticket: ticket).encoded_id).to eq(encoded_id)
+    end
+
+    it 'will raise an exception if one is not found' do
+      expect { described_class.find_current_identifier_for(ticket: ticket) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
