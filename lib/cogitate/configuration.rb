df1cf8683ac525182a809bfdfecefca0a820a533
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-13 08:54:31 -0400

    Adding configuration for remote server

--- lib/cogitate/configuration.rb
+++ lib/cogitate/configuration.rb
@@ -1,14 +1,19 @@
 module Cogitate
   # Responsible for containing the configuration information for Cogitate
   class Configuration
-    # :nodoc:
+    # If something is not properly configured
     class ConfigurationError < RuntimeError
       def initialize(method_name)
         super("Cogitate::Configuration##{method_name} has not been set")
       end
     end
 
-    CONFIG_ATTRIBUTE_NAMES = [:tokenizer_password, :tokenizer_encryption_type, :tokenizer_issuer_claim].freeze
+    CONFIG_ATTRIBUTE_NAMES = [
+      :remote_server_base_url,
+      :tokenizer_password,
+      :tokenizer_encryption_type,
+      :tokenizer_issuer_claim
+    ].freeze
     private_constant :CONFIG_ATTRIBUTE_NAMES
 
     def initialize(**keywords)
@@ -17,20 +22,35 @@ module Cogitate
       end
     end
 
-    attr_writer :tokenizer_password
-    attr_writer :tokenizer_encryption_type
-    attr_writer :tokenizer_issuer_claim
-
-    def tokenizer_password
-      @tokenizer_password || fail(ConfigurationError, :tokenizer_password)
+    CONFIG_ATTRIBUTE_NAMES.each do |method_name|
+      attr_writer method_name
+      define_method(method_name) do
+        instance_variable_get("@#{method_name}") || fail(ConfigurationError, method_name)
+      end
     end
 
-    def tokenizer_encryption_type
-      @tokenizer_encryption_type || fail(ConfigurationError, :tokenizer_password)
-    end
-
-    def tokenizer_issuer_claim
-      @tokenizer_issuer_claim || fail(ConfigurationError, :tokenizer_password)
-    end
+    # !@attribute [rw] remote_server_base_url
+    #   @return [String] What is the URL of the Cogitate server you want to connect to?
+    # !@attribute [rw] tokenizer_password
+    #   What is the tokenizer password you are going to be using to:
+    #   * create a token (i.e. a private RSA key)
+    #   * decode a token (i.e. a public RSA key)
+    #
+    #   If you are implemnting a Cogitate client, you'll want to use the public key
+    #   @return [String]
+    #   @see Cogitate::Services::Tokenizer
+    # !@attribute [rw] tokenizer_encryption_type
+    #   What is the encryption type for the tokenizer. You will need to ensure that
+    #   the Cogitate server and client are using the same encryption mechanism.
+    #   @return [String]
+    #   @example `configuration.tokenizer_encryption_type = 'RS256'`
+    #   @see Cogitate::Services::Tokenizer
+    # !@attribute [rw] tokenizer_issuer_claim
+    #   As per JSON Web Token specification, what is the Issuer Claim
+    #   the Cogitate server and client are using the same encryption mechanism.
+    #   @return [String]
+    #   @example `configuration.tokenizer_issuer_claim = 'https://library.nd.edu'`
+    #   @see https://tools.ietf.org/html/rfc7519#section-4.1.1
+    #   @see https://github.com/jwt/ruby-jwt#issuer-claim
   end
 end
--- spec/lib/cogitate/configuration_spec.rb
+++ spec/lib/cogitate/configuration_spec.rb
@@ -2,6 +2,15 @@ require 'cogitate/configuration'
 
 RSpec.describe Cogitate::Configuration do
   subject { described_class.new }
+  context '#remote_server_base_url' do
+    it 'will use the set value' do
+      subject.remote_server_base_url = 'hello'
+      expect(subject.remote_server_base_url).to eq('hello')
+    end
+    it 'will raise an exception if not set' do
+      expect { subject.remote_server_base_url }.to raise_error(described_class::ConfigurationError)
+    end
+  end
   context '#tokenizer_password' do
     it 'will use the set value' do
       subject.tokenizer_password = 'hello'
