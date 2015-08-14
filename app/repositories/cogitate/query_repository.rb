dae241e846dbe1ecb79d47b7ce898216cb71c691
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-08-14 10:35:51 -0400

    Wordsmithing
    [skip ci]

--- app/repositories/cogitate/query_repository.rb
+++ app/repositories/cogitate/query_repository.rb
@@ -5,13 +5,19 @@ module Cogitate
   class QueryRepository
     GROUP_STRATEGY = 'group'.freeze
 
+    # @api private
     # TODO: Given that this repository could be multi-purpose, is it better to pass the collaborator
     #       as a parameter to the method that would use it?
+    # @param identifier_relationship_repository [#each_identifier_related_to]
     def initialize(identifier_relationship_repository: default_identifier_relationship_repository)
       self.identifier_relationship_repository = identifier_relationship_repository
     end
 
     # @api public
+    #
+    # Responsible for exposing a means of iteration on verified groups related to the given identifier
+    #
+    # @param identifier [Cogitate::Interfaces::Identifier]
     def with_verified_group_identifier_related_to(identifier:)
       return enum_for(:with_verified_group_identifier_related_to, identifier: identifier) unless block_given?
       identifier_relationship_repository.each_identifier_related_to(identifier: identifier, strategy: GROUP_STRATEGY) do |group_identifier|
@@ -21,6 +27,7 @@ module Cogitate
     end
 
     # @api private I'm not yet certain if I should yield a group for which we have no record or if I can yield a stub of information
+    # @param identifier [Cogitate::Interfaces::Identifier]
     def with_verified_existing_group_for(identifier:)
       return enum_for(:with_verified_existing_group_for, identifier: identifier) unless block_given?
       group = Group.find_by(id: identifier.identifying_value)
@@ -34,6 +41,7 @@ module Cogitate
 
     attr_accessor :identifier_relationship_repository
 
+    # @api private
     def default_identifier_relationship_repository
       require 'repository_service/identifier_relationship' unless defined?(RepositoryService::IdentifierRelationship)
       RepositoryService::IdentifierRelationship
