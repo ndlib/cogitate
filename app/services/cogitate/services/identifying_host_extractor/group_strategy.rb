4c480b25aa33bf795eb425635b35178e699abb6f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-09-03 11:00:15 -0400

    Adding GroupStrategy extraction strategy

---
+++ app/services/cogitate/services/identifying_host_extractor/group_strategy.rb
@@ -0,0 +1,53 @@
+require 'cogitate/interfaces'
+module Cogitate
+  module Services
+    module IdentifyingHostExtractor
+      # Responsible for extracting the host that will oversee the visitation of the agent
+      class GroupStrategy
+        extend Contracts
+
+        Contract(Contracts::KeywordArgs[identifier: Cogitate::Interfaces::IdentifierInterface] => Cogitate::Interfaces::HostInterface)
+        # @api public
+        def self.call(identifier:)
+          new(identifier: identifier)
+        end
+
+        def initialize(identifier:, repository: default_repository, member_visitation_service: default_member_visitation_service)
+          self.identifier = identifier
+          self.repository = repository
+          self.member_visitation_service = member_visitation_service
+        end
+
+        def invite(guest)
+          guest.visit(identifier) { |visitor| receive(visitor) }
+          guest
+        end
+
+        private
+
+        attr_accessor :identifier, :repository, :member_visitation_service
+
+        def receive(visitor)
+          identifier_is_verified = false
+          repository.with_verified_existing_group_for(identifier: identifier) do |verified_identifier|
+            identifier_is_verified = true
+            visitor.add_identifier(verified_identifier)
+            visitor.add_verified_identifier(verified_identifier)
+            member_visitation_service.call(identifier: verified_identifier, visitor: visitor)
+          end
+          visitor.add_identifier(identifier) unless identifier_is_verified
+        end
+
+        def default_repository
+          require 'cogitate/query_repository' unless defined?(Cogitate::QueryRepository)
+          Cogitate::QueryRepository.new
+        end
+
+        def default_member_visitation_service
+          # TODO: a placeholder
+          ->(*) {}
+        end
+      end
+    end
+  end
+end
---
+++ spec/services/cogitate/services/identifying_host_extractor/group_strategy_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_fast_helper'
+require 'cogitate/services/identifying_host_extractor/group_strategy'
+require 'cogitate/models/identifier'
+
+RSpec.describe Cogitate::Services::IdentifyingHostExtractor::GroupStrategy do
+  let(:identifier) { Cogitate::Models::Identifier.new(strategy: 'group', identifying_value: 'a group') }
+  let(:member_visitation_service) { double(call: true) }
+  let(:repository) { double(with_verified_existing_group_for: true) }
+  let(:is_verified) { true }
+  subject { described_class.new(identifier: identifier, repository: repository, member_visitation_service: member_visitation_service) }
+
+  its(:default_member_visitation_service) { should respond_to(:call) }
+  its(:default_repository) { should respond_to(:with_verified_existing_group_for) }
+
+  include Cogitate::RSpecMatchers
+  it 'will expose .call as a convenience method' do
+    expect(described_class.call(identifier: identifier)).to contractually_honor(Cogitate::Interfaces::HostInterface)
+  end
+
+  let(:guest) { double(visit: true) }
+  let(:visitor) { double(add_identifier: true, add_verified_identifier: true) }
+  before { allow(guest).to receive(:visit).and_yield(visitor) }
+
+  context 'group is a verified group' do
+    before { expect(repository).to receive(:with_verified_existing_group_for).with(identifier: identifier).and_yield(identifier) }
+    it 'will tell the visitor to add the identifier as an unverified identifier' do
+      subject.invite(guest)
+      expect(visitor).to have_received(:add_identifier).with(identifier)
+    end
+    it 'will tell the visitor to add the identifier as a verified identifier' do
+      subject.invite(guest)
+      expect(visitor).to have_received(:add_verified_identifier).with(identifier)
+    end
+    it 'will call the member_visitation_service' do
+      subject.invite(guest)
+      expect(member_visitation_service).to have_received(:call).with(identifier: identifier, visitor: visitor)
+    end
+  end
+
+  context 'group is not a verified group' do
+    before { expect(repository).to receive(:with_verified_existing_group_for).with(identifier: identifier) }
+    it 'will tell the visitor to add the identifier as an unverified identifier' do
+      subject.invite(guest)
+      expect(visitor).to have_received(:add_identifier).with(identifier)
+    end
+    it 'will NOT tell the visitor to add the identifier as an verified identifier' do
+      subject.invite(guest)
+      expect(visitor).to_not have_received(:add_verified_identifier)
+    end
+    it 'will NOT call the member_visitation_service' do
+      subject.invite(guest)
+      expect(member_visitation_service).to_not have_received(:call)
+    end
+  end
+end
