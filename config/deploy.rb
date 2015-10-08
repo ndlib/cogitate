a1a1df1f3e2ee0360d0c42e19b924a784d8b0685
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-10-08 14:03:12 -0400

    Copying deploy.rb from Mark's previous work

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,107 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+# config valid only for Capistrano 3.1
+lock '3.4.0'
+set :default_env, {
+  path: "/opt/ruby/current/bin:$PATH"
+}
+set :bundle_roles, [:app, :work]
+set :bundle_flags, "--deployment --path=vendor/bundle"
+set :bundle_cmd, "/opt/ruby/current/bin/bundle"
+set :bundle_without, %w{development test documentation}.join(' ')
+set :application, 'cogitate'
+set :scm, :git
+set :repo_url, "https://github.com/ndlib/cogitate.git"
+set :branch, ENV['BRANCH_NAME'] || 'master'
+set :keep_releases, 5
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :secret_repo_name, Proc.new{
+  case fetch(:rails_env)
+  when 'staging' then 'secret_staging'
+  when 'pre_production' then 'secret_pprd'
+  when 'production' then 'secret_prod'
+  end
+}
 
 namespace :deploy do
+
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute "touch #{release_path}/tmp/restart.txt"
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  task :db_create do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:create"
+        end
+      end
     end
   end
 
-  after :finishing, 'deploy:cleanup'
+  desc 'Perform data migrations via db/data'
+  task :data_migrate do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:data:migrate"
+        end
+      end
+    end
+  end
+
+  desc 'Perform db migrations via db/migrate'
+  task :db_migrate do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:migrate"
+        end
+      end
+    end
+  end
+
+  desc 'Precompile assets as we are not checking in a version'
+  task :precompile_assets do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "assets:precompile"
+         end
+       end
+     end
+   end
+
+  desc 'Prepare for a dog and pony show on the staging server'
+  task :reset_the_dog_and_pony_show do
+    on roles(:app) do
+      within release_path do
+        with rails_env: 'staging' do
+          execute :rake, "db:drop db:create db:schema:load db:seed cogitate:environment_bootstrapper"
+        end
+      end
+    end
+  end
 end
+
+namespace :configuration do
+  task :copy_secrets do
+    on roles(:app) do
+      within release_path do
+        execute "export PATH=/opt/ruby/current/bin:$PATH && cd #{release_path} && sh scripts/update_secrets.sh #{fetch(:secret_repo_name)}"
+      end
+    end
+  end
+end
+
+before 'deploy:db_migrate', 'configuration:copy_secrets'
+after 'deploy', 'deploy:db_migrate'
+after 'deploy:db_migrate', 'deploy:precompile_assets'
+after 'deploy', 'deploy:cleanup'
+after 'deploy', 'deploy:restart'
+
+require './config/boot'
+require 'airbrake/capistrano3'
+require 'capistrano/passenger'
