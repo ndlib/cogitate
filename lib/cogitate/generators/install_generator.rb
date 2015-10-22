931d52cea7d69f5bc2a5ec4ef267f6dafb78fd75
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-10-22 10:51:57 -0500

    Fixing an undocumented module
    ```console
    lib/cogitate/generators/install_generator.rb -- 1 warning:
    [3]:Commitment::InstallGenerator has no descriptive comment
      (IrresponsibleModule)
    ```

--- lib/cogitate/generators/install_generator.rb
+++ lib/cogitate/generators/install_generator.rb
@@ -1,5 +1,6 @@
-module Commitment
-  # :nodoc:
+module Cogitate
+  # Responsible for the installation of Cogitate into another Rails
+  # application.
   class InstallGenerator < Rails::Generators::Base
     source_root(File.expand_path("../templates", __FILE__))
 
