6a6aaeec044c7435b9d6fa24db97e034bb0e0fa6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-10-22 17:50:06 -0500

    Addressing an IrresponsibleModule code reek
    ```console
    lib/cogitate/client/response_parsers.rb -- 1 warning:
      [6]:Cogitate::Client::ResponseParsers has no descriptive comment
      (IrresponsibleModule)
    ```

--- lib/cogitate/client/response_parsers.rb
+++ lib/cogitate/client/response_parsers.rb
@@ -2,7 +2,7 @@ require 'cogitate/client/exceptions'
 
 module Cogitate
   module Client
-    # Responsibl
+    # Responsible for finding the correct parser based on the given name.
     module ResponseParsers
       def self.fetch(name)
         return name if name.respond_to?(:call)
