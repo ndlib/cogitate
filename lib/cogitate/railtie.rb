965b4bfc6d9ec684fc10b546424dfc574bf82735
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2015-10-22 10:57:16 -0500

    Fixing a reek
    ```console
    lib/cogitate/railtie.rb -- 1 warning:
      [5]:Cogitate::Railtie has no descriptive comment (IrresponsibleModule)
    ```

--- lib/cogitate/railtie.rb
+++ lib/cogitate/railtie.rb
@@ -1,7 +1,8 @@
 require 'rails/railtie'
 
 module Cogitate
-  # :nodoc:
+  # Responsible for exposing features (generators, rake tasks, etc) to a Rails
+  # application that leverages Cogitate.
   class Railtie < Rails::Railtie
     generators do
       require 'cogitate/generators/install_generator'
